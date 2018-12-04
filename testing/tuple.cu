f737c7e7e937531f1e899741d70cd531b5894458
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2018-12-03 21:46:27 -0800

    More 10.1 testing enhancements:
    * Add `<thrust/limits.h>` and `thrust::numeric_limits`, which we can specialize
      for types like `custom_numeric`.
    * Add `truncate_to_max_representable` utility for avoiding the generation of
      ranges that cannot be represented by the underlying element type in generic
      unit test code.
    * Add `DECLARE_INTEGRAL_VARIABLE_UNITTEST` test declaration macro.
    * Add `DECLARE_VARIABLE_UNITTEST_WITH_TYPES_AND_NAME` test declaration macro.
    
    Bug 2455943
    Bug 2017697

--- testing/tuple.cu
+++ testing/tuple.cu
@@ -108,7 +108,7 @@ struct TestTupleConstructor
     ASSERT_EQUAL(data[9], get<9>(t10));*/
   }
 };
-SimpleUnitTest<TestTupleConstructor, RandomizableTypes> TestTupleConstructorInstance;
+SimpleUnitTest<TestTupleConstructor, TriviallyRelocatableTypes> TestTupleConstructorInstance;
 
 template <typename T>
 struct TestMakeTuple
@@ -196,7 +196,7 @@ struct TestMakeTuple
     ASSERT_EQUAL(data[9], get<9>(t10));*/
   }
 };
-SimpleUnitTest<TestMakeTuple, RandomizableTypes> TestMakeTupleInstance;
+SimpleUnitTest<TestMakeTuple, TriviallyRelocatableTypes> TestMakeTupleInstance;
 
 template <typename T>
 struct TestTupleGet
@@ -283,7 +283,7 @@ struct TestTupleGet
     ASSERT_EQUAL(data[9], thrust::get<9>(t10));*/
   }
 };
-SimpleUnitTest<TestTupleGet, RandomizableTypes> TestTupleGetInstance;
+SimpleUnitTest<TestTupleGet, TriviallyRelocatableTypes> TestTupleGetInstance;
 
 
 
