4bc3fba62bd1a2cf8d362af45061128d2d9e1657
Istvan Kiss <istvan@streamhpc.com>
2019-02-08 12:47:41 +0100

    Implemented pair tests

---
+++ test/test_assertations.hpp
@@ -0,0 +1,73 @@
+// Google Test
+#include <gtest/gtest.h>
+
+//#include <thrust/complex.h>
+
+testing::AssertionResult EqFailureQuite(const char* lhs_expression,
+                                        const char* rhs_expression,
+                                        bool ignoring_case) {
+  testing::Message msg;
+  msg << "Expressions during equality check:";
+  msg << "\n  " << lhs_expression;
+  msg << "\n  " << rhs_expression;
+
+  if (ignoring_case) {
+    msg << "\nIgnoring case";
+  }
+
+  return testing::AssertionFailure() << msg;
+}
+
+testing::AssertionResult CmpHelperEQFailureQuite(const char* lhs_expression,
+                                                 const char* rhs_expression) {
+  return EqFailureQuite(lhs_expression,
+                        rhs_expression,
+                        false);
+}
+
+template <typename T1, typename T2>
+testing::AssertionResult CmpHelperEQ(const char* lhs_expression,
+                                     const char* rhs_expression,
+                                     const T1& lhs,
+                                     const T2& rhs) {
+  if (lhs == rhs) {
+    return testing::AssertionSuccess();
+  }
+
+  return CmpHelperEQFailureQuite(lhs_expression, rhs_expression);
+}
+
+#define ASSERT_EQ_QUIET(val1, val2)\
+  ASSERT_PRED_FORMAT2(CmpHelperEQ, \
+                      val1, val2)
+
+
+/*template <typename T>
+testing::AssertionResult ComplexNearPredFormat(const char* expr1,
+                                               const char* expr2,
+                                               const char* abs_error_expr,
+                                               thrust::complex<T> val1,
+                                               thrust::complex<T> val2,
+                                               thrust::complex<double> abs_error){
+  const thrust::complex<double> diff(fabs((double)val1.real() - (double)val2.real())
+                                    ,fabs((double)val1.imag() - (double)val2.imag()));
+  if (diff.real() <= abs_error.real() &&
+      diff.imag() <= abs_error.imag())
+    return testing::AssertionSuccess();
+
+  return testing::AssertionFailure()
+      << "The difference between " << expr1 << " and " << expr2
+      << " is " << diff << ", which exceeds " << abs_error_expr << ", where\n"
+      << expr1 << " evaluates to " << val1 << ",\n"
+      << expr2 << " evaluates to " << val2 << ", and\n"
+      << abs_error_expr << " evaluates to " << abs_error << ".";
+}
+
+#define ASSERT_NEAR_COMPLEX_ERROR(val1, val2, abs_error)\
+  ASSERT_PRED_FORMAT3(ComplexNearPredFormat<typename decltype(val1)::value_type>, \
+                      val1, val2, abs_error)
+
+
+#define ASSERT_NEAR_COMPLEX(val1, val2)\
+  ASSERT_NEAR_COMPLEX_ERROR(val1, val2, thrust::complex<T>(std::numeric_limits<T>::epsilon(),std::numeric_limits<T>::epsilon()))
+*/
