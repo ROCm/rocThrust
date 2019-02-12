faaf4f89180b93c8fb8eb382b538a9614d3ff2c9
Istvan Kiss <istvan@streamhpc.com>
2019-02-12 17:27:39 +0100

    Extended for_each and copy tests

--- test/test_utils.hpp
+++ test/test_utils.hpp
@@ -131,3 +131,56 @@ class my_system : public thrust::device_execution_policy<my_system>
 };
 
 struct my_tag : thrust::device_execution_policy<my_tag> {};
+
+template <typename T, unsigned int N>
+struct FixedVector
+{
+    T data[N];
+
+    __host__ __device__
+    FixedVector()
+    {
+        for(unsigned int i = 0; i < N; i++)
+            data[i] = T();
+    }
+
+    __host__ __device__
+    FixedVector(T init)
+    {
+        for(unsigned int i = 0; i < N; i++)
+            data[i] = init;
+    }
+
+    __host__ __device__
+    FixedVector operator+(const FixedVector& bs) const
+    {
+        FixedVector output;
+        for(unsigned int i = 0; i < N; i++)
+            output.data[i] = data[i] + bs.data[i];
+        return output;
+    }
+
+    __host__ __device__
+    bool operator<(const FixedVector& bs) const
+    {
+        for(unsigned int i = 0; i < N; i++)
+        {
+            if(data[i] < bs.data[i])
+                return true;
+            else if(bs.data[i] < data[i])
+                return false;
+        }
+        return false;
+    }
+
+    __host__ __device__
+    bool operator==(const FixedVector& bs) const
+    {
+        for(unsigned int i = 0; i < N; i++)
+        {
+            if(!(data[i] == bs.data[i]))
+                return false;
+        }
+        return true;
+    }
+};
