ec890f25553df07678f0bc7c1dfd096e402b4fbf
Istvan Kiss <istvan@streamhpc.com>
2019-04-17 17:41:43 +0200

    License update of system hip

--- thrust/system/hip/detail/util.h
+++ thrust/system/hip/detail/util.h
@@ -1,5 +1,6 @@
 /******************************************************************************
  * Copyright (c) 2016, NVIDIA CORPORATION.  All rights meserved.
+ *  Modifications Copyright© 2019 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
