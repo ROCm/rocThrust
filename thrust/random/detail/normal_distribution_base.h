180df3483c6a2a55e035a3e9efe1fc3f5efa3038
Istvan Kiss <istvan@streamhpc.com>
2019-05-03 19:06:08 +0200

    Rocm cmake package
    [ROCm/rocThrust commit: e992c3e4891f9d51f71d6042b7e70d29b9c8178e]

--- thrust/random/detail/normal_distribution_base.h
+++ thrust/random/detail/normal_distribution_base.h
@@ -1,5 +1,6 @@
 /*
  *  Copyright 2008-2013 NVIDIA Corporation
+ *  Modifications Copyright© 2019 Advanced Micro Devices, Inc. All rights reserved. 
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -151,4 +152,3 @@ template<typename RealType>
 } // end detail
 } // end random
 } // end thrust
-
