a3f9edb7ad839ef0b2d853abfd6964adb74538d7
Istvan Kiss <istvan@streamhpc.com>
2019-05-10 09:52:17 +0200

    Update test test_scatter to test_zip_iterator_sort_by_key

--- test/test_utils.hpp
+++ test/test_utils.hpp
@@ -14,157 +14,155 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
- 
+
 #pragma once
 
-#include <iostream>
-#include <type_traits>
-#include <cstdlib>
-#include <algorithm>
-#include <random>
-#include <vector>
-
-#include <thrust/host_vector.h>
 #include <thrust/execution_policy.h>
+#include <thrust/host_vector.h>
+
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <random>
+#include <type_traits>
+#include <vector>
 
 std::vector<size_t> get_sizes()
 {
-  std::vector<size_t> sizes = {
-    0, 1, 2, 12, 63, 64, 211, 256, 344,
-    1024, 2048, 5096, 34567, (1 << 17) - 1220, 1000000, (1 << 20) - 123
-  };
-  return sizes;
+    std::vector<size_t> sizes = {
+        0, 1, 2, 12, 63, 64, 211, 256, 344,
+        1024, 2048, 5096, 34567, (1 << 17) - 1220, 1000000, (1 << 20) - 123
+    };
+    return sizes;
 }
 
-template<class T>
-inline auto get_random_data(size_t size, T, T)
-  -> typename std::enable_if<std::is_same<T,bool>::value, thrust::host_vector<T>>::type
+template <class T>
+inline auto get_random_data(size_t size, T, T) ->
+    typename std::enable_if<std::is_same<T, bool>::value, thrust::host_vector<T>>::type
 {
-  std::random_device rd;
-  std::default_random_engine gen(rd());
-  std::bernoulli_distribution distribution(0.5);
-  thrust::host_vector<T> data(size);
-  std::generate(data.begin(), data.end(), [&]() { return distribution(gen); });
-  return data;
+    std::random_device          rd;
+    std::default_random_engine  gen(rd());
+    std::bernoulli_distribution distribution(0.5);
+    thrust::host_vector<T>      data(size);
+    std::generate(data.begin(), data.end(), [&]() { return distribution(gen); });
+    return data;
 }
 
-template<class T>
-inline auto get_random_data(size_t size, T min, T max)
-  -> typename std::enable_if<rocprim::is_integral<T>::value && !std::is_same<T,bool>::value, thrust::host_vector<T>>::type
+template <class T>
+inline auto get_random_data(size_t size, T min, T max) ->
+    typename std::enable_if<rocprim::is_integral<T>::value && !std::is_same<T, bool>::value,
+                            thrust::host_vector<T>>::type
 {
-  std::random_device rd;
-  std::default_random_engine gen(rd());
+    std::random_device               rd;
+    std::default_random_engine       gen(rd());
     std::uniform_int_distribution<T> distribution(min, max);
-  thrust::host_vector<T> data(size);
-  std::generate(data.begin(), data.end(), [&]() { return distribution(gen); });
-  return data;
+    thrust::host_vector<T>           data(size);
+    std::generate(data.begin(), data.end(), [&]() { return distribution(gen); });
+    return data;
 }
 
-template<class T>
-inline auto get_random_data(size_t size, T min, T max)
-  -> typename std::enable_if<rocprim::is_floating_point<T>::value, thrust::host_vector<T>>::type
+template <class T>
+inline auto get_random_data(size_t size, T min, T max) ->
+    typename std::enable_if<rocprim::is_floating_point<T>::value, thrust::host_vector<T>>::type
 {
-  std::random_device rd;
-  std::default_random_engine gen(rd());
-  std::uniform_real_distribution<T> distribution(min, max);
-  thrust::host_vector<T> data(size);
-  std::generate(data.begin(), data.end(), [&]() { return distribution(gen); });
-  return data;
+    std::random_device                rd;
+    std::default_random_engine        gen(rd());
+    std::uniform_real_distribution<T> distribution(min, max);
+    thrust::host_vector<T>            data(size);
+    std::generate(data.begin(), data.end(), [&]() { return distribution(gen); });
+    return data;
 }
 
-template<class T>
+template <class T>
 struct custom_compare_less
 {
-  __host__ __device__
-  bool operator()(const T &lhs, const T &rhs) const
-  {
-    return lhs < rhs;
-  }
+    __host__ __device__ bool operator()(const T& lhs, const T& rhs) const
+    {
+        return lhs < rhs;
+    }
 }; // end less
 
 struct user_swappable
 {
-  inline __host__ __device__
-  user_swappable(bool swapped = false)
-    : was_swapped(swapped)
-  {}
+    inline __host__ __device__ user_swappable(bool swapped = false)
+        : was_swapped(swapped)
+    {
+    }
 
-  bool was_swapped;
+    bool was_swapped;
 };
 
-inline __host__ __device__
-bool operator==(const user_swappable &x, const user_swappable &y)
+inline __host__ __device__ bool operator==(const user_swappable& x, const user_swappable& y)
 {
-  return x.was_swapped == y.was_swapped;
+    return x.was_swapped == y.was_swapped;
 }
 
-inline __host__ __device__
-void swap(user_swappable &x, user_swappable &y)
+inline __host__ __device__ void swap(user_swappable& x, user_swappable& y)
 {
-  x.was_swapped = true;
-  y.was_swapped = false;
+    x.was_swapped = true;
+    y.was_swapped = false;
 }
 
 class my_system : public thrust::device_execution_policy<my_system>
 {
-  public:
+public:
     my_system(int)
-      : correctly_dispatched(false),
-        num_copies(0)
-    {}
+        : correctly_dispatched(false)
+        , num_copies(0)
+    {
+    }
 
-    my_system(const my_system &other)
-      : correctly_dispatched(false),
-        num_copies(other.num_copies + 1)
-    {}
+    my_system(const my_system& other)
+        : correctly_dispatched(false)
+        , num_copies(other.num_copies + 1)
+    {
+    }
 
     void validate_dispatch()
     {
-      correctly_dispatched = (num_copies == 0);
+        correctly_dispatched = (num_copies == 0);
     }
 
     bool is_valid()
     {
-      return correctly_dispatched;
+        return correctly_dispatched;
     }
 
-  private:
+private:
     bool correctly_dispatched;
 
     // count the number of copies so that we can validate
     // that dispatch does not introduce any
     unsigned int num_copies;
 
-
     // disallow default construction
     my_system();
 };
 
-struct my_tag : thrust::device_execution_policy<my_tag> {};
+struct my_tag : thrust::device_execution_policy<my_tag>
+{
+};
 
 template <typename T, unsigned int N>
 struct FixedVector
 {
     T data[N];
 
-    __host__ __device__
-    FixedVector()
+    __host__ __device__ FixedVector()
     {
         #pragma nounroll
         for(unsigned int i = 0; i < N; i++)
             data[i] = T();
     }
 
-    __host__ __device__
-    FixedVector(T init)
+    __host__ __device__ FixedVector(T init)
     {
         #pragma nounroll
         for(unsigned int i = 0; i < N; i++)
             data[i] = init;
     }
 
-    __host__ __device__
-    FixedVector operator+(const FixedVector& bs) const
+    __host__ __device__ FixedVector operator+(const FixedVector& bs) const
     {
         FixedVector output;
         #pragma nounroll
@@ -173,8 +171,7 @@ struct FixedVector
         return output;
     }
 
-    __host__ __device__
-    bool operator<(const FixedVector& bs) const
+    __host__ __device__ bool operator<(const FixedVector& bs) const
     {
         #pragma nounroll
         for(unsigned int i = 0; i < N; i++)
@@ -187,8 +184,7 @@ struct FixedVector
         return false;
     }
 
-    __host__ __device__
-    bool operator==(const FixedVector& bs) const
+    __host__ __device__ bool operator==(const FixedVector& bs) const
     {
         #pragma nounroll
         for(unsigned int i = 0; i < N; i++)
--- test/test_zip_iterator_scan.cpp
+++ test/test_zip_iterator_scan.cpp
@@ -15,7 +15,6 @@
  *  limitations under the License.
  */
 
-// Thrust
 #include <thrust/iterator/zip_iterator.h>
 #include <thrust/scan.h>
 
@@ -23,96 +22,91 @@
 
 TESTS_DEFINE(ZipIteratorScanVariablesTests, NumericalTestsParams);
 
-template<typename Tuple>
+template <typename Tuple>
 struct TuplePlus
 {
-  __host__ __device__
-  Tuple operator()(Tuple x, Tuple y) const
-  {
-    using namespace thrust;
-    return make_tuple(get<0>(x) + get<0>(y),
-                      get<1>(x) + get<1>(y));
-  }
+    __host__ __device__ Tuple operator()(Tuple x, Tuple y) const
+    {
+        using namespace thrust;
+        return make_tuple(get<0>(x) + get<0>(y), get<1>(x) + get<1>(y));
+    }
 }; // end SumTuple
-
 
 TYPED_TEST(ZipIteratorScanVariablesTests, TestZipIteratorScan)
 {
-  using T = typename TestFixture::input_type;
+    using T = typename TestFixture::input_type;
 
-  const std::vector<size_t> sizes = get_sizes();
-  for(auto size : sizes)
-  {
-    using namespace thrust;
+    const std::vector<size_t> sizes = get_sizes();
+    for(auto size : sizes)
+    {
+        using namespace thrust;
 
-    thrust::host_vector<T> h_data0 = get_random_data<T>(size,
-                                                        std::numeric_limits<T>::min(),
-                                                        std::numeric_limits<T>::max());
-    thrust::host_vector<T> h_data1 = get_random_data<T>(size,
-                                                        std::numeric_limits<T>::min(),
-                                                        std::numeric_limits<T>::max());
+        thrust::host_vector<T> h_data0 = get_random_data<T>(
+            size, std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
+        thrust::host_vector<T> h_data1 = get_random_data<T>(
+            size, std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
 
-    device_vector<T> d_data0 = h_data0;
-    device_vector<T> d_data1 = h_data1;
+        device_vector<T> d_data0 = h_data0;
+        device_vector<T> d_data1 = h_data1;
 
-    typedef tuple<T,T> Tuple;
+        typedef tuple<T, T> Tuple;
 
-    host_vector<Tuple>   h_result(size);
-    device_vector<Tuple> d_result(size);
+        host_vector<Tuple>   h_result(size);
+        device_vector<Tuple> d_result(size);
 
-    // inclusive_scan (tuple output)
-    inclusive_scan( make_zip_iterator(make_tuple(h_data0.begin(), h_data1.begin())),
-                    make_zip_iterator(make_tuple(h_data0.end(),   h_data1.end())),
-                    h_result.begin(),
-                    TuplePlus<Tuple>());
-    inclusive_scan( make_zip_iterator(make_tuple(d_data0.begin(), d_data1.begin())),
-                    make_zip_iterator(make_tuple(d_data0.end(),   d_data1.end())),
-                    d_result.begin(),
-                    TuplePlus<Tuple>());
-    ASSERT_EQ_QUIET(h_result, d_result);
+        // inclusive_scan (tuple output)
+        inclusive_scan(make_zip_iterator(make_tuple(h_data0.begin(), h_data1.begin())),
+                       make_zip_iterator(make_tuple(h_data0.end(), h_data1.end())),
+                       h_result.begin(),
+                       TuplePlus<Tuple>());
+        inclusive_scan(make_zip_iterator(make_tuple(d_data0.begin(), d_data1.begin())),
+                       make_zip_iterator(make_tuple(d_data0.end(), d_data1.end())),
+                       d_result.begin(),
+                       TuplePlus<Tuple>());
+        ASSERT_EQ_QUIET(h_result, d_result);
 
-    // exclusive_scan (tuple output)
-    exclusive_scan( make_zip_iterator(make_tuple(h_data0.begin(), h_data1.begin())),
-                    make_zip_iterator(make_tuple(h_data0.end(),   h_data1.end())),
-                    h_result.begin(),
-                    make_tuple<T,T>(0,0),
-                    TuplePlus<Tuple>());
-    exclusive_scan( make_zip_iterator(make_tuple(d_data0.begin(), d_data1.begin())),
-                    make_zip_iterator(make_tuple(d_data0.end(),   d_data1.end())),
-                    d_result.begin(),
-                    make_tuple<T,T>(0,0),
-                    TuplePlus<Tuple>());
-    ASSERT_EQ_QUIET(h_result, d_result);
+        // exclusive_scan (tuple output)
+        exclusive_scan(make_zip_iterator(make_tuple(h_data0.begin(), h_data1.begin())),
+                       make_zip_iterator(make_tuple(h_data0.end(), h_data1.end())),
+                       h_result.begin(),
+                       make_tuple<T, T>(0, 0),
+                       TuplePlus<Tuple>());
+        exclusive_scan(make_zip_iterator(make_tuple(d_data0.begin(), d_data1.begin())),
+                       make_zip_iterator(make_tuple(d_data0.end(), d_data1.end())),
+                       d_result.begin(),
+                       make_tuple<T, T>(0, 0),
+                       TuplePlus<Tuple>());
+        ASSERT_EQ_QUIET(h_result, d_result);
 
-    host_vector<T>   h_result0(size);
-    host_vector<T>   h_result1(size);
-    device_vector<T> d_result0(size);
-    device_vector<T> d_result1(size);
+        host_vector<T>   h_result0(size);
+        host_vector<T>   h_result1(size);
+        device_vector<T> d_result0(size);
+        device_vector<T> d_result1(size);
 
-    // inclusive_scan (zip_iterator output)
-    inclusive_scan( make_zip_iterator(make_tuple(h_data0.begin(), h_data1.begin())),
-                    make_zip_iterator(make_tuple(h_data0.end(),   h_data1.end())),
-                    make_zip_iterator(make_tuple(h_result0.begin(), h_result1.begin())),
-                    TuplePlus<Tuple>());
-    inclusive_scan( make_zip_iterator(make_tuple(d_data0.begin(), d_data1.begin())),
-                    make_zip_iterator(make_tuple(d_data0.end(),   d_data1.end())),
-                    make_zip_iterator(make_tuple(d_result0.begin(), d_result1.begin())),
-                    TuplePlus<Tuple>());
-    ASSERT_EQ_QUIET(h_result0, d_result0);
-    ASSERT_EQ_QUIET(h_result1, d_result1);
+        // inclusive_scan (zip_iterator output)
+        inclusive_scan(make_zip_iterator(make_tuple(h_data0.begin(), h_data1.begin())),
+                       make_zip_iterator(make_tuple(h_data0.end(), h_data1.end())),
+                       make_zip_iterator(make_tuple(h_result0.begin(), h_result1.begin())),
+                       TuplePlus<Tuple>());
+        inclusive_scan(make_zip_iterator(make_tuple(d_data0.begin(), d_data1.begin())),
+                       make_zip_iterator(make_tuple(d_data0.end(), d_data1.end())),
+                       make_zip_iterator(make_tuple(d_result0.begin(), d_result1.begin())),
+                       TuplePlus<Tuple>());
+        ASSERT_EQ_QUIET(h_result0, d_result0);
+        ASSERT_EQ_QUIET(h_result1, d_result1);
 
-    // exclusive_scan (zip_iterator output)
-    exclusive_scan( make_zip_iterator(make_tuple(h_data0.begin(), h_data1.begin())),
-                    make_zip_iterator(make_tuple(h_data0.end(),   h_data1.end())),
-                    make_zip_iterator(make_tuple(h_result0.begin(), h_result1.begin())),
-                    make_tuple<T,T>(0,0),
-                    TuplePlus<Tuple>());
-    exclusive_scan( make_zip_iterator(make_tuple(d_data0.begin(), d_data1.begin())),
-                    make_zip_iterator(make_tuple(d_data0.end(),   d_data1.end())),
-                    make_zip_iterator(make_tuple(d_result0.begin(), d_result1.begin())),
-                    make_tuple<T,T>(0,0),
-                    TuplePlus<Tuple>());
-    ASSERT_EQ_QUIET(h_result0, d_result0);
-    ASSERT_EQ_QUIET(h_result1, d_result1);
-  }
+        // exclusive_scan (zip_iterator output)
+        exclusive_scan(make_zip_iterator(make_tuple(h_data0.begin(), h_data1.begin())),
+                       make_zip_iterator(make_tuple(h_data0.end(), h_data1.end())),
+                       make_zip_iterator(make_tuple(h_result0.begin(), h_result1.begin())),
+                       make_tuple<T, T>(0, 0),
+                       TuplePlus<Tuple>());
+        exclusive_scan(make_zip_iterator(make_tuple(d_data0.begin(), d_data1.begin())),
+                       make_zip_iterator(make_tuple(d_data0.end(), d_data1.end())),
+                       make_zip_iterator(make_tuple(d_result0.begin(), d_result1.begin())),
+                       make_tuple<T, T>(0, 0),
+                       TuplePlus<Tuple>());
+        ASSERT_EQ_QUIET(h_result0, d_result0);
+        ASSERT_EQ_QUIET(h_result1, d_result1);
+    }
 }
