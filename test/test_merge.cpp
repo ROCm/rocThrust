c72d533647d62b2d7169f040b9aca02fc249c395
Istvan Kiss <istvan@streamhpc.com>
2019-05-10 09:43:50 +0200

    Update test test_header to scan_by_key
    [ROCm/rocThrust commit: 4f740af698b0ea297bbe26c947398f11df89d7bb]

--- test/test_merge.cpp
+++ test/test_merge.cpp
@@ -15,16 +15,15 @@
  *  limitations under the License.
  */
 
-// Thrust
 #include <thrust/device_vector.h>
-#include <thrust/host_vector.h>
-#include <thrust/merge.h>
-#include <thrust/functional.h>
-#include <thrust/sort.h>
-#include <thrust/unique.h>
 #include <thrust/extrema.h>
+#include <thrust/functional.h>
+#include <thrust/host_vector.h>
 #include <thrust/iterator/discard_iterator.h>
 #include <thrust/iterator/retag.h>
+#include <thrust/merge.h>
+#include <thrust/sort.h>
+#include <thrust/unique.h>
 
 #include "test_header.hpp"
 
@@ -33,92 +32,82 @@ TESTS_DEFINE(PrimitiveMergeTests, NumericalTestsParams);
 
 TEST(MergeTests, UsingHip)
 {
-  ASSERT_EQ(THRUST_DEVICE_SYSTEM, THRUST_DEVICE_SYSTEM_HIP);
+    ASSERT_EQ(THRUST_DEVICE_SYSTEM, THRUST_DEVICE_SYSTEM_HIP);
 }
 
 TYPED_TEST(MergeTests, MergeSimple)
 {
-  using Vector = typename TestFixture::input_type;
-  using Iterator = typename Vector::iterator;
+    using Vector   = typename TestFixture::input_type;
+    using Iterator = typename Vector::iterator;
 
-  Vector a(3), b(4);
+    Vector a(3), b(4);
 
-  a[0] = 0; a[1] = 2; a[2] = 4;
-  b[0] = 0; b[1] = 3; b[2] = 3; b[3] = 4;
+    a[0] = 0;
+    a[1] = 2;
+    a[2] = 4;
+    b[0] = 0;
+    b[1] = 3;
+    b[2] = 3;
+    b[3] = 4;
 
-  Vector ref(7);
-  ref[0] = 0;
-  ref[1] = 0;
-  ref[2] = 2;
-  ref[3] = 3;
-  ref[4] = 3;
-  ref[5] = 4;
-  ref[6] = 4;
+    Vector ref(7);
+    ref[0] = 0;
+    ref[1] = 0;
+    ref[2] = 2;
+    ref[3] = 3;
+    ref[4] = 3;
+    ref[5] = 4;
+    ref[6] = 4;
 
-  Vector result(7);
+    Vector result(7);
 
-  Iterator end = thrust::merge(a.begin(), a.end(),
-                               b.begin(), b.end(),
-                               result.begin());
+    Iterator end = thrust::merge(a.begin(), a.end(), b.begin(), b.end(), result.begin());
 
-  EXPECT_EQ(result.end(), end);
-  ASSERT_EQ(ref, result);
+    EXPECT_EQ(result.end(), end);
+    ASSERT_EQ(ref, result);
 }
 
-template<typename InputIterator1,
-         typename InputIterator2,
-         typename OutputIterator>
-OutputIterator merge(my_system &system,
+template <typename InputIterator1, typename InputIterator2, typename OutputIterator>
+OutputIterator merge(my_system& system,
                      InputIterator1,
                      InputIterator1,
                      InputIterator2,
                      InputIterator2,
                      OutputIterator result)
 {
-  system.validate_dispatch();
-  return result;
+    system.validate_dispatch();
+    return result;
 }
 
 TEST(MergeTests, MergeDispatchExplicit)
 {
-  thrust::device_vector<int> vec(1);
+    thrust::device_vector<int> vec(1);
 
-  my_system sys(0);
-  thrust::merge(sys,
-                vec.begin(),
-                vec.begin(),
-                vec.begin(),
-                vec.begin(),
-                vec.begin());
+    my_system sys(0);
+    thrust::merge(sys, vec.begin(), vec.begin(), vec.begin(), vec.begin(), vec.begin());
 
-  ASSERT_EQ(true, sys.is_valid());
+    ASSERT_EQ(true, sys.is_valid());
 }
 
-template<typename InputIterator1,
-         typename InputIterator2,
-         typename OutputIterator>
-OutputIterator merge(my_tag,
-                     InputIterator1,
-                     InputIterator1,
-                     InputIterator2,
-                     InputIterator2,
-                     OutputIterator result)
+template <typename InputIterator1, typename InputIterator2, typename OutputIterator>
+OutputIterator merge(
+    my_tag, InputIterator1, InputIterator1, InputIterator2, InputIterator2, OutputIterator result)
 {
-  *result = 13;
-  return result;
+    *result = 13;
+    return result;
 }
 
 TEST(MergeTests, MergeDispatchImplicit)
 {
-  thrust::device_vector<int> vec(1);
+    thrust::device_vector<int> vec(1);
 
-  thrust::merge(thrust::retag<my_tag>(vec.begin()),
-                thrust::retag<my_tag>(vec.begin()),
-                thrust::retag<my_tag>(vec.begin()),
-                thrust::retag<my_tag>(vec.begin()),
-                thrust::retag<my_tag>(vec.begin()));
+    thrust::merge(thrust::retag<my_tag>(vec.begin()),
+                  thrust::retag<my_tag>(vec.begin()),
+                  thrust::retag<my_tag>(vec.begin()),
+                  thrust::retag<my_tag>(vec.begin()),
+                  thrust::retag<my_tag>(vec.begin()));
 
-  ASSERT_EQ(13, vec.front());
+    ASSERT_EQ(13, vec.front());
 }
 
 TYPED_TEST(PrimitiveMergeTests, MergeWithRandomData)
@@ -131,7 +120,10 @@ TYPED_TEST(PrimitiveMergeTests, MergeWithRandomData)
         size_t expanded_sizes[]   = {0, 1, size / 2, size, size + 1, 2 * size};
         size_t num_expanded_sizes = sizeof(expanded_sizes) / sizeof(size_t);
 
-        thrust::host_vector<T> random = get_random_data<unsigned short int>(size + *thrust::max_element(expanded_sizes, expanded_sizes + num_expanded_sizes), 0, 255);
+        thrust::host_vector<T> random = get_random_data<unsigned short int>(
+            size + *thrust::max_element(expanded_sizes, expanded_sizes + num_expanded_sizes),
+            0,
+            255);
         thrust::host_vector<T> h_a(random.begin(), random.begin() + size);
         thrust::host_vector<T> h_b(random.begin() + size, random.end());
 
@@ -141,7 +133,7 @@ TYPED_TEST(PrimitiveMergeTests, MergeWithRandomData)
         thrust::device_vector<T> d_a = h_a;
         thrust::device_vector<T> d_b = h_b;
 
-        for (size_t i = 0; i < num_expanded_sizes; i++)
+        for(size_t i = 0; i < num_expanded_sizes; i++)
         {
             size_t expanded_size = expanded_sizes[i];
 
@@ -151,14 +143,12 @@ TYPED_TEST(PrimitiveMergeTests, MergeWithRandomData)
             typename thrust::host_vector<T>::iterator   h_end;
             typename thrust::device_vector<T>::iterator d_end;
 
-            h_end = thrust::merge(h_a.begin(), h_a.end(),
-                                    h_b.begin(), h_b.begin() + expanded_size,
-                                    h_result.begin());
+            h_end = thrust::merge(
+                h_a.begin(), h_a.end(), h_b.begin(), h_b.begin() + expanded_size, h_result.begin());
             h_result.resize(h_end - h_result.begin());
 
-            d_end = thrust::merge(d_a.begin(), d_a.end(),
-                                    d_b.begin(), d_b.begin() + expanded_size,
-                                    d_result.begin());
+            d_end = thrust::merge(
+                d_a.begin(), d_a.end(), d_b.begin(), d_b.begin() + expanded_size, d_result.begin());
             d_result.resize(d_end - d_result.begin());
 
             thrust::host_vector<T> d_result_h = d_result;
@@ -174,12 +164,10 @@ TYPED_TEST(PrimitiveMergeTests, MergeToDiscardIterator)
     const std::vector<size_t> sizes = get_sizes();
     for(auto size : sizes)
     {
-        thrust::host_vector<T> h_a = get_random_data<T>(size,
-                                                       std::numeric_limits<T>::min(),
-                                                       std::numeric_limits<T>::max());
-        thrust::host_vector<T> h_b = get_random_data<T>(size,
-                                                       std::numeric_limits<T>::min(),
-                                                       std::numeric_limits<T>::max());
+        thrust::host_vector<T> h_a = get_random_data<T>(
+            size, std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
+        thrust::host_vector<T> h_b = get_random_data<T>(
+            size, std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
 
         thrust::stable_sort(h_a.begin(), h_a.end());
         thrust::stable_sort(h_b.begin(), h_b.end());
@@ -187,15 +175,11 @@ TYPED_TEST(PrimitiveMergeTests, MergeToDiscardIterator)
         thrust::device_vector<T> d_a = h_a;
         thrust::device_vector<T> d_b = h_b;
 
-        thrust::discard_iterator<> h_result =
-        thrust::merge(h_a.begin(), h_a.end(),
-                        h_b.begin(), h_b.end(),
-                        thrust::make_discard_iterator());
+        thrust::discard_iterator<> h_result = thrust::merge(
+            h_a.begin(), h_a.end(), h_b.begin(), h_b.end(), thrust::make_discard_iterator());
 
-        thrust::discard_iterator<> d_result =
-        thrust::merge(d_a.begin(), d_a.end(),
-                        d_b.begin(), d_b.end(),
-                        thrust::make_discard_iterator());
+        thrust::discard_iterator<> d_result = thrust::merge(
+            d_a.begin(), d_a.end(), d_b.begin(), d_b.end(), thrust::make_discard_iterator());
 
         thrust::discard_iterator<> reference(2 * size);
 
@@ -211,12 +195,10 @@ TYPED_TEST(PrimitiveMergeTests, MergeDescending)
     const std::vector<size_t> sizes = get_sizes();
     for(auto size : sizes)
     {
-        thrust::host_vector<T> h_a = get_random_data<T>(size,
-                                                       std::numeric_limits<T>::min(),
-                                                       std::numeric_limits<T>::max());
-        thrust::host_vector<T> h_b = get_random_data<T>(size,
-                                                       std::numeric_limits<T>::min(),
-                                                       std::numeric_limits<T>::max());
+        thrust::host_vector<T> h_a = get_random_data<T>(
+            size, std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
+        thrust::host_vector<T> h_b = get_random_data<T>(
+            size, std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
 
         thrust::stable_sort(h_a.begin(), h_a.end(), thrust::greater<T>());
         thrust::stable_sort(h_b.begin(), h_b.end(), thrust::greater<T>());
@@ -224,21 +206,17 @@ TYPED_TEST(PrimitiveMergeTests, MergeDescending)
         thrust::device_vector<T> d_a = h_a;
         thrust::device_vector<T> d_b = h_b;
 
-        thrust::host_vector<T> h_result(h_a.size() + h_b.size());
+        thrust::host_vector<T>   h_result(h_a.size() + h_b.size());
         thrust::device_vector<T> d_result(d_a.size() + d_b.size());
 
-        typename thrust::host_vector<T>::iterator h_end;
+        typename thrust::host_vector<T>::iterator   h_end;
         typename thrust::device_vector<T>::iterator d_end;
 
-        h_end = thrust::merge(h_a.begin(), h_a.end(),
-                            h_b.begin(), h_b.end(),
-                            h_result.begin(),
-                            thrust::greater<T>());
+        h_end = thrust::merge(
+            h_a.begin(), h_a.end(), h_b.begin(), h_b.end(), h_result.begin(), thrust::greater<T>());
 
-        d_end = thrust::merge(d_a.begin(), d_a.end(),
-                            d_b.begin(), d_b.end(),
-                            d_result.begin(),
-                            thrust::greater<T>());
+        d_end = thrust::merge(
+            d_a.begin(), d_a.end(), d_b.begin(), d_b.end(), d_result.begin(), thrust::greater<T>());
 
         ASSERT_EQ(h_result, d_result);
     }
--- test/test_pair_sort.cpp
+++ test/test_pair_sort.cpp
@@ -15,10 +15,9 @@
  *  limitations under the License.
  */
 
-// Thrust
 #include <thrust/pair.h>
-#include <thrust/sort.h>
 #include <thrust/sequence.h>
+#include <thrust/sort.h>
 
 #include "test_header.hpp"
 
@@ -26,49 +25,49 @@ TESTS_DEFINE(PairSortTests, NumericalTestsParams);
 
 struct make_pair_functor
 {
-  template<typename T1, typename T2>
-  __host__ __device__
-    thrust::pair<T1,T2> operator()(const T1 &x, const T2 &y)
-  {
-    return thrust::make_pair(x,y);
-  } // end operator()()
+    template <typename T1, typename T2>
+    __host__ __device__ thrust::pair<T1, T2> operator()(const T1& x, const T2& y)
+    {
+        return thrust::make_pair(x, y);
+    } // end operator()()
 }; // end make_pair_functor
 
 TYPED_TEST(PairSortTests, TestPairStableSortByKey)
 {
-  using T = typename TestFixture::input_type;
-  using P = thrust::pair<T,T>;
+    using T = typename TestFixture::input_type;
+    using P = thrust::pair<T, T>;
 
-  const std::vector<size_t> sizes = get_sizes();
-  for(auto size : sizes)
-  {
-    thrust::host_vector<T> h_p1 = get_random_data<T>(size,
-                                                     std::numeric_limits<T>::min(),
-                                                     std::numeric_limits<T>::max());;
+    const std::vector<size_t> sizes = get_sizes();
+    for(auto size : sizes)
+    {
+        thrust::host_vector<T> h_p1 = get_random_data<T>(
+            size, std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
+        ;
 
-    thrust::host_vector<T> h_p2 = get_random_data<T>(size,
-                                                     std::numeric_limits<T>::min(),
-                                                     std::numeric_limits<T>::max());;
+        thrust::host_vector<T> h_p2 = get_random_data<T>(
+            size, std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
+        ;
 
-    thrust::host_vector<P>   h_pairs(size);
+        thrust::host_vector<P> h_pairs(size);
 
-    thrust::host_vector<int> h_values(size);
-    thrust::sequence(h_values.begin(), h_values.end());
+        thrust::host_vector<int> h_values(size);
+        thrust::sequence(h_values.begin(), h_values.end());
 
-    // zip up pairs on the host
-    thrust::transform(h_p1.begin(), h_p1.end(), h_p2.begin(), h_pairs.begin(), make_pair_functor());
+        // zip up pairs on the host
+        thrust::transform(
+            h_p1.begin(), h_p1.end(), h_p2.begin(), h_pairs.begin(), make_pair_functor());
 
-    // device arrays
-    thrust::device_vector<P>   d_pairs = h_pairs;
-    thrust::device_vector<int> d_values = h_values;
+        // device arrays
+        thrust::device_vector<P>   d_pairs  = h_pairs;
+        thrust::device_vector<int> d_values = h_values;
 
-    // sort on the host
-    thrust::stable_sort_by_key(h_pairs.begin(), h_pairs.end(), h_values.begin());
+        // sort on the host
+        thrust::stable_sort_by_key(h_pairs.begin(), h_pairs.end(), h_values.begin());
 
-    // sort on the device
-    thrust::stable_sort_by_key(d_pairs.begin(), d_pairs.end(), d_values.begin());
+        // sort on the device
+        thrust::stable_sort_by_key(d_pairs.begin(), d_pairs.end(), d_values.begin());
 
-    ASSERT_EQ_QUIET(h_pairs,  d_pairs);
-    ASSERT_EQ_QUIET(h_values, d_values);
-  }
+        ASSERT_EQ_QUIET(h_pairs, d_pairs);
+        ASSERT_EQ_QUIET(h_values, d_values);
+    }
 }
