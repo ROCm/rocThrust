2eea91703792f45888252db2884049db2e6d3de3
Saad Rahim <saad.rahim@amd.com>
2019-05-30 12:47:24 -0700

    Changing doxy

--- doc/run_doxygen.sh
+++ doc/run_doxygen.sh
@@ -4,9 +4,10 @@ if [ -d docBin ]; then
     rm -rf docBin
 fi
 
-rm nccl.h
+#rm nccl.h
 
-sed -e 's/ROCFFT_EXPORT //g' ../src/nccl.h.in > nccl.h
-doxygen Doxyfile
+#sed -e 's/ROCFFT_EXPORT //g' ../src/nccl.h.in > nccl.h
+../doxygen doc/thrust.dox
+#Doxyfile
 #rm nccl.h
 
