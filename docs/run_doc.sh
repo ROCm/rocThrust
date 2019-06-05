72f1b3a1698752de5667b75d6f3d838c482335d8
Saad Rahim <saad.rahim@amd.com>
2019-06-05 14:20:14 -0700

    cleaning

--- docs/run_doc.sh
+++ docs/run_doc.sh
@@ -4,13 +4,7 @@ if [ -d docBin ]; then
     rm -rf docBin
 fi
 
-#sed -e 's/ROCFFT_EXPORT //g' ../library/include/rocfft.h > rocfft.h
-../doxygen doc/thrust.dox
+doxygen thrust.dox
 
-cd source
 make clean
 make html
-cd ..
-
-#rm rocfft.h
-
