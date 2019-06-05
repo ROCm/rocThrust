7b6d35f937e8ee36fff287b776e49efed36222e4
Saad Rahim <saad.rahim@amd.com>
2019-06-05 14:22:27 -0700

    cleaning

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ import subprocess
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
-    subprocess.call('cd ..; ./run_doc.sh; cd source', shell=True)
+    subprocess.call('./run_doc.sh;', shell=True)
 
 # -- General configuration ------------------------------------------------
 
