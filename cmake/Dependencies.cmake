44d3b5b8b3186db929581a9e7804f3eccf3fc9fc
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2019-12-23 15:39:33 -0700

    Change dependency to rocPRIM from master to develop (#82)

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -24,7 +24,7 @@ set(ROCPRIM_ROOT ${CMAKE_CURRENT_BINARY_DIR}/rocPRIM CACHE PATH "")
 download_project(
   PROJ                rocPRIM
   GIT_REPOSITORY      https://github.com/ROCmSoftwarePlatform/rocPRIM.git
-  GIT_TAG             master
+  GIT_TAG             develop
   INSTALL_DIR         ${ROCPRIM_ROOT}
   CMAKE_ARGS          -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_PREFIX_PATH=/opt/rocm
   LOG_DOWNLOAD        TRUE
