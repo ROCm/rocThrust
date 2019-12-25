94abd59244fd960d6b7c53854c78dffb6528a9df
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2019-12-24 17:16:09 -0700

    Split Jenkinsfile into Common, PreCheckin, and NPI (#83)
    [ROCm/rocThrust commit: c180fbeda787e69196f6447983d494dbff7c0208]

---
+++ .jenkins/PreCheckin.groovy
@@ -0,0 +1,49 @@
+#!/usr/bin/env groovy
+// This shared library is available at https://github.com/ROCmSoftwarePlatform/rocJENKINS/
+@Library('rocJenkins') _
+
+// This file is for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+import com.amd.project.*
+import com.amd.docker.*
+import java.nio.file.Path;
+
+rocThrustCI:
+{
+
+    def rocthrust = new rocProject('rocThrust', 'PreCheckin')
+    // customize for project
+    rocthrust.paths.build_command = './install'
+
+    // Define test architectures, optional rocm version argument is available
+    def nodes = new dockerNodes(['ubuntu && gfx803', 'sles && gfx906', 'gfx900 && centos7', 'gfx906 && centos7'], rocthrust)
+
+    boolean formatCheck = false
+
+    def commonGroovy
+
+    def compileCommand =
+    {
+        platform, project->
+
+        commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/Common.groovy"
+        commonGroovy.runCompileCommand(platform, project)
+    }
+
+    def testCommand =
+    {
+        platform, project->
+
+        commonGroovy.runTestCommand(platform, project)
+    }
+
+    def packageCommand =
+    {
+        platform, project->
+        
+        commonGroovy.runPackageCommand(platform, project)
+    }
+
+    buildProject(rocthrust, formatCheck, nodes.dockerArray, compileCommand, testCommand, packageCommand)
+}
