89edc0c9cf32eaeb0a470c432bc87e2fc247657c
Eiden Yoshida <eiden.yoshida@amd.com>
2020-01-06 13:51:48 -0700

    Update rocPRIM tag

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -24,7 +24,7 @@ set(ROCPRIM_ROOT ${CMAKE_CURRENT_BINARY_DIR}/rocPRIM CACHE PATH "")
 download_project(
   PROJ                rocPRIM
   GIT_REPOSITORY      https://github.com/ROCmSoftwarePlatform/rocPRIM.git
-  GIT_TAG             develop
+  GIT_TAG             88b03e36e13d17a7d3e07d954f59ba31b02f032d
   INSTALL_DIR         ${ROCPRIM_ROOT}
   CMAKE_ARGS          -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_PREFIX_PATH=/opt/rocm
   LOG_DOWNLOAD        TRUE
