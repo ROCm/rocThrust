f4f07e827d79442fcd037d53b3775da5e9fc501b
Michał 'Griwes' Dominiak <griwes@griwes.info>
2020-01-20 18:03:45 +0100

    Changes to satisfy GCC's -Wdeprecated-copy in the test suite.
    * Add the missing of copy constructor or copy assignment operator to all
    classes GCC complains about in the warning, for C++11 only (since the
    deprecation is C++11 and up), with the default meaning that they had
    since forever.
    * Add missing move operations to cuda::vector (how did *that* happen?).
    
    Bug 200582781
    
    Reviewed-by: Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>

--- examples/uninitialized_vector.cu
+++ examples/uninitialized_vector.cu
@@ -33,6 +33,10 @@ template<typename T>
   uninitialized_allocator & operator=(const uninitialized_allocator &) = default;
 #endif
 
+#if THRUST_CPP_DIALECT >= 2011
+  uninitialized_allocator & operator=(const uninitialized_allocator &) = default;
+#endif
+
   // for correctness, you should also redefine rebind when you inherit
   // from an allocator type; this way, if the allocator is rebound somewhere,
   // it's going to be rebound to the correct type - and not to its base
