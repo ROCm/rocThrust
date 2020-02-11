f38a3d17014b8b390df41e53ddd6e5d566d2ea5c
saadrahim <44449863+saadrahim@users.noreply.github.com>
2020-02-10 17:57:51 -0700

    Switching to expanded CI test suite (#92)

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -1,29 +1,20 @@
 // This file is for internal AMD use.
 // If you are interested in running your own Jenkins, please raise a github issue for assistance.
 
-def runCompileCommand(platform, project)
+def runCompileCommand(platform, project, jobName)
 {
     project.paths.construct_build_prefix()
-        
-    def command 
 
-    if(platform.jenkinsLabel.contains('hip-clang'))
-    { 
-        command = """#!/usr/bin/env bash
+    String compiler = jobName.contains('hipclang') ? 'hipcc' : 'hcc'
+    String hipClangArgs = jobName.contains('hipclang') ? '--hip-clang' : ''
+
+    command = """#!/usr/bin/env bash
                 set -x
                 cd ${project.paths.project_build_prefix}
-                LD_LIBRARY_PATH=/opt/rocm/hcc/lib CXX=/opt/rocm/bin/hipcc ${project.paths.build_command} --hip-clang
-            """
-    }
-    else
-    {
-        command = """#!/usr/bin/env bash
-                set -x
-                cd ${project.paths.project_build_prefix}
-                LD_LIBRARY_PATH=/opt/rocm/hcc/lib CXX=/opt/rocm/bin/hcc ${project.paths.build_command} -c
+                LD_LIBRARY_PATH=/opt/rocm/hcc/lib ${project.paths.build_command} ${hipClangArgs} 
             """
 
-    }
+    
     platform.runCommand(this, command)
 }
 
@@ -41,11 +32,11 @@ def runTestCommand (platform, project)
     platform.runCommand(this, command)
 }
 
-def runPackageCommand(platform, project)
+def runPackageCommand(platform, project, jobName)
 {
     def command
         
-    if(platform.jenkinsLabel.contains('hip-clang'))
+    if(jobName.contains('hipclang'))
     {
         packageCommand = null
     }
