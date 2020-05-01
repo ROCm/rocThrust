98ed7f3d1354a40f76c768c223ebe01e2ed8e63c
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2020-04-30 20:58:07 -0700

    Fix the one-policy overload of `thrust::async::copy` to not copy the policy, because this leads to use-after-move in the future dependency extraction and stream stealing code. Fixes #1115.

--- thrust/async/copy.h
+++ thrust/async/copy.h
@@ -107,15 +107,20 @@ struct copy_fn final
   , ForwardIt&& first, Sentinel&& last
   , OutputIt&& output
   )
-  // ADL dispatch.
-  THRUST_DECLTYPE_RETURNS(
-    async_copy(
+//  THRUST_DECLTYPE_RETURNS(
+  { return
+    copy_fn::call(
       thrust::detail::derived_cast(thrust::detail::strip_const(exec))
-    , thrust::detail::derived_cast(thrust::detail::strip_const(exec))
+      // Synthesize a suitable new execution policy, because we don't want to
+      // try and extract twice from the one we were passed.
+    , typename remove_cvref_t<
+        decltype(thrust::detail::derived_cast(thrust::detail::strip_const(exec)))
+      >::tag_type{}
     , THRUST_FWD(first), THRUST_FWD(last)
     , THRUST_FWD(output)
     )
-  )
+    ; }
+//  )
 
   template <typename ForwardIt, typename Sentinel, typename OutputIt>
   __host__
