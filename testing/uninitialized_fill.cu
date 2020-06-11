2924884fb0f157cae970f817dad3949d0756d5dd
Istvan Kiss <istvan@streamhpc.com>
2020-06-11 16:13:16 +0200

    Update uninitialized_fill test
    [ROCm/rocThrust commit: 73e92ce8b48af8afeb7544db3ff5894ef36f7740]

--- testing/uninitialized_fill.cu
+++ testing/uninitialized_fill.cu
@@ -164,6 +164,7 @@ DECLARE_VECTOR_UNITTEST(TestUninitializedFillPOD);
 
 struct CopyConstructTest
 {
+  __host__ __device__ 
   CopyConstructTest(void)
     :copy_constructed_on_host(false),
      copy_constructed_on_device(false)
