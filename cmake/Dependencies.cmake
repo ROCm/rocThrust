6555b3a34bcce175da52fe0a7b2c2758128a879e
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2021-02-26 12:51:48 +0100

    Add capability to provide external rocRAND installs

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -18,8 +18,8 @@ if (NOT Git_FOUND)
   message(FATAL_ERROR "Please ensure Git is installed on the system")
 endif()
 
-# rocPRIM (https://github.com/ROCmSoftwarePlatform/rocPRIM)
 if(NOT DEPENDENCIES_FORCE_DOWNLOAD)
+  # rocPRIM (https://github.com/ROCmSoftwarePlatform/rocPRIM)
   find_package(rocprim)
 endif()
 if(NOT rocprim_FOUND)
@@ -43,8 +43,8 @@ endif()
 
 # Test dependencies
 if(BUILD_TEST)
-  # Google Test (https://github.com/google/googletest)
   if(NOT DEPENDENCIES_FORCE_DOWNLOAD)
+    # Google Test (https://github.com/google/googletest)
     find_package(GTest QUIET)
   endif()
 
@@ -66,26 +66,30 @@ if(BUILD_TEST)
     )
   endif()
   find_package(GTest REQUIRED CONFIG PATHS ${GTEST_ROOT})
-endif()
 
-# rocRAND (https://github.com/ROCmSoftwarePlatform/rocRAND)
-if(LARGE_TEST)
-  find_package(rocrand QUIET)
-  if(NOT rocrand_FOUND)
-    message(STATUS "Downloading and building rocrand.")
-    download_project(
-      PROJ                rocrand
-      GIT_REPOSITORY      https://github.com/ROCmSoftwarePlatform/rocRAND.git
-      GIT_TAG             develop
-      INSTALL_DIR         ${CMAKE_CURRENT_BINARY_DIR}/deps/rocrand
-      CMAKE_ARGS          -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_PREFIX_PATH=/opt/rocm
-      LOG_DOWNLOAD        TRUE
-      LOG_CONFIGURE       TRUE
-      LOG_BUILD           TRUE
-      LOG_INSTALL         TRUE
-      BUILD_PROJECT       TRUE
-      UPDATE_DISCONNECTED TRUE # Never update automatically from the remote repository
-    )
-    find_package(rocrand REQUIRED CONFIG PATHS ${CMAKE_CURRENT_BINARY_DIR}/deps/rocrand NO_DEFAULT_PATH)
+  if(LARGE_TEST)
+    # rocRAND (https://github.com/ROCmSoftwarePlatform/rocRAND)
+    if(NOT DEPENDENCIES_FORCE_DOWNLOAD)
+      find_package(rocrand QUIET)
+    endif()
+
+    if(NOT rocrand_FOUND)
+      message(STATUS "rocRAND not found or force download rocRAND on. Downloading and building rocrand.")
+      set(ROCRAND_ROOT ${CMAKE_CURRENT_BINARY_DIR}/deps/rocrand CACHE PATH "")
+      download_project(
+        PROJ                rocrand
+        GIT_REPOSITORY      https://github.com/ROCmSoftwarePlatform/rocRAND.git
+        GIT_TAG             develop
+        INSTALL_DIR         ${ROCRAND_ROOT}
+        CMAKE_ARGS          -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_PREFIX_PATH=/opt/rocm
+        LOG_DOWNLOAD        TRUE
+        LOG_CONFIGURE       TRUE
+        LOG_BUILD           TRUE
+        LOG_INSTALL         TRUE
+        BUILD_PROJECT       TRUE
+        UPDATE_DISCONNECTED TRUE # Never update automatically from the remote repository
+      )
+    endif()
+    find_package(rocrand REQUIRED CONFIG PATHS ${ROCRAND_ROOT} NO_DEFAULT_PATH)
   endif()
 endif()
