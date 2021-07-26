0510b5f818c54948f216147c5a8ca5a0e9be2710
Liam Wrubleski <Liam.Wrubleski@amd.com>
2021-07-26 12:15:12 -0600

    Setup runtime and development packages (#184)
    * Basic changes for devel package
    
    * Update rocm-cmake version & build
    
    [ROCm/rocThrust commit: 22fda4adfced5be5addec35b16050d7ffc99a67e]

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -60,3 +60,31 @@ if(BUILD_TEST)
   )
   find_package(GTest REQUIRED)
 endif()
+
+# Find or download/install rocm-cmake project
+find_package(ROCM 0.6 QUIET CONFIG PATHS /opt/rocm)
+if(NOT ROCM_FOUND)
+  message(STATUS "rocm-cmake not found. Downloading and building rocm-cmake.")
+  set(ROCM_CMAKE_ROOT ${CMAKE_CURRENT_BINARY_DIR}/deps/rocm-cmake CACHE PATH "")
+  download_project(
+      PROJ           rocm-cmake
+      GIT_REPOSITORY https://github.com/RadeonOpenCompute/rocm-cmake.git
+      GIT_TAG        master
+      INSTALL_DIR    ${ROCM_CMAKE_ROOT}
+      CMAKE_ARGS     -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+      LOG_DOWNLOAD   TRUE
+      LOG_CONFIGURE  TRUE
+      LOG_BUILD      TRUE
+      LOG_INSTALL    TRUE
+      BUILD_PROJECT  TRUE
+      ${UPDATE_DISCONNECTED_IF_AVAILABLE}
+    )
+  find_package(ROCM 0.6 REQUIRED CONFIG PATHS ${ROCM_CMAKE_ROOT})
+endif()
+
+include( ROCMSetupVersion )
+include( ROCMCreatePackage )
+include( ROCMInstallTargets )
+include( ROCMPackageConfigHelpers )
+include( ROCMInstallSymlinks )
+include( ROCMCheckTargetIds OPTIONAL )
