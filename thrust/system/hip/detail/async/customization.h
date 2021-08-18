014859650d1861a99cec4fb8b453466b09faba5d
Istvan Kiss <istvan@streamhpc.com>
2021-08-18 15:05:26 +0200

    Update copyright years

--- thrust/system/hip/detail/async/customization.h
+++ thrust/system/hip/detail/async/customization.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
- * Modifications Copyright© 2020 Advanced Micro Devices, Inc. All rights reserved.
+ * Modifications Copyright© 2020-2021 Advanced Micro Devices, Inc. All rights reserved.
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
