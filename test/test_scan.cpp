54c7cad2630c9b42cf7d8af655f4aea29e6e59f6
Istvan Kiss <istvan@streamhpc.com>
2021-10-26 23:17:24 +0200

    Disable scan mixed type tests

--- test/test_scan.cpp
+++ test/test_scan.cpp
@@ -323,12 +323,13 @@ TEST(ScanTests, TestScanMixedTypesHost)
     TestScanMixedTypes<thrust::host_vector<int>, thrust::host_vector<float>>();
 }
 
-TEST(ScanTests, TestScanMixedTypesDevice)
+// TODO: Fix mixed type tests
+/*TEST(ScanTests, TestScanMixedTypesDevice)
 {
     SCOPED_TRACE(testing::Message() << "with device_id= " << test::set_device_from_ctest());
 
     TestScanMixedTypes<thrust::device_vector<int>, thrust::device_vector<float>>();
-}
+}*/
 
 TYPED_TEST(ScanVariablesTests, TestScanWithOperator)
 {
