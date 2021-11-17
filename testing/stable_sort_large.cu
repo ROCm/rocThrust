6ae96999a34fa0ec27ac812c116ac7db146296b1
Gergely Meszaros <gergely@streamhpc.com>
2021-11-17 10:23:18 +0000

    Disable a large stable sort test, that would use too much shared memory

--- testing/stable_sort_large.cu
+++ testing/stable_sort_large.cu
@@ -48,8 +48,8 @@ void TestStableSortWithLargeKeys(void)
     _TestStableSortWithLargeKeys<int,   16>();
     _TestStableSortWithLargeKeys<int,   32>();
     _TestStableSortWithLargeKeys<int,   64>();
-    _TestStableSortWithLargeKeys<int,  128>();
 #if THRUST_DEVICE_SYSTEM != THRUST_DEVICE_SYSTEM_HIP
+//    _TestStableSortWithLargeKeys<int,  128>();
 //    _TestStableSortWithLargeKeys<int,  256>();
 //    _TestStableSortWithLargeKeys<int,  512>();
 //    _TestStableSortWithLargeKeys<int, 1024>();
