a7db271fd0d9bba585c5039452885ef887ff1324
Sergey Kostrov <117676396+SergiyKostrov@users.noreply.github.com>
2022-11-21 11:21:27 -0700

    Update common.groovy
    CTest3 is No longer used.

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -31,7 +31,8 @@ def runTestCommand (platform, project)
     String sudo = auxiliary.sudo(platform.jenkinsLabel)
     String centos = platform.jenkinsLabel.contains('centos') ? '3' : ''
 
-    def testCommand = "ctest${centos} --output-on-failure"
+    def extraArgs = ""
+    def testCommand = "ctest $(extraArgs) --output-on-failure"
     def hmmTestCommand = ''
     def excludeRegex = 'reduce_by_key.hip'
     
