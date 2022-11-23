fa8db258fd2b37c768d2ec52ed8618d1e55b1343
Sergey Kostrov <117676396+SergiyKostrov@users.noreply.github.com>
2022-11-23 10:11:18 -0700

    Update common.groovy

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -31,7 +31,7 @@ def runTestCommand (platform, project)
     String sudo = auxiliary.sudo(platform.jenkinsLabel)
     String centos = platform.jenkinsLabel.contains('centos') ? '3' : ''
 
-    def extraArgs = ""
+    def extraArgs = ''
     def testCommand = "ctest $(extraArgs) --output-on-failure"
     def hmmTestCommand = ''
     def excludeRegex = 'reduce_by_key.hip'
