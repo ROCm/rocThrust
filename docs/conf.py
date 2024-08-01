ce90a575f22f5918fb769993e20ea755d2728f49
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-08-01 17:20:04 -0600

    Use rocm-docs-core as extension

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,8 @@
 
 import re
 
-from rocm_docs import ROCmDocs
-with open('../CMakeLists.txt', encoding='utf-8') as f:
-    match = re.search(r'rocm_setup_version\(VERSION\s+\"?([0-9.]+)[^0-9.]+', f.read())
+with open("../CMakeLists.txt", encoding="utf-8") as f:
+    match = re.search(r"rocm_setup_version\(VERSION\s+\"?([0-9.]+)[^0-9.]+", f.read())
     if not match:
         raise ValueError("VERSION not found!")
     version_number = match[1]
@@ -23,11 +22,15 @@ release = version_number
 
 external_toc_path = "./sphinx/_toc.yml"
 
-docs_core = ROCmDocs(left_nav_title)
-docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
-docs_core.setup()
+extensions = ["rocm_docs", "rocm_docs.doxygen", "sphinxcontrib.doxylink"]
+html_theme = "rocm_docs_theme"
+html_theme_options = {"flavor": "rocm"}
 
 external_projects_current_project = "rocthrust"
 
-for sphinx_var in ROCmDocs.SPHINX_VARS:
-    globals()[sphinx_var] = getattr(docs_core, sphinx_var)
+doxygen_root = "doxygen"
+doxygen_project = {
+    "name": project,
+    "path": "doxygen/xml",
+}
+doxysphinx_enabled = True
