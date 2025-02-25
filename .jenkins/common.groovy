34760ab5b533f0906b80a8b57274a0c43069b8ee
amd-garydeng <garydeng@amd.com>
2025-02-25 13:12:29 -0700

    move to right dir

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -79,6 +79,7 @@ def runTestCommand (platform, project, boolean rocmExamples=false)
                     set -ex
                     cd ${project.paths.project_build_prefix}/build/release/package
                     ${buildString}
+                    cd ../../..
                     testDirs=("Libraries/rocThrust")
                     git clone https://github.com/ROCm/rocm-examples.git
                     rocm_examples_dir=\$(readlink -f rocm-examples)
