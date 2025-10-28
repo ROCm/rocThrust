ad450b629b513dab105d1d388c57327023ef800d
Nara <18581205+Naraenda@users.noreply.github.com>
2025-10-28 19:21:31 +0000

    [rocm-libraries] ROCm/rocm-libraries#1812 (commit ff5a2d4)
    build: for primitive and rand libs change depedency on 'ROCM'
     package to be 'ROCmCMakeBuildTools' (#1812)
    MIME-Version: 1.0
    Content-Type: text/plain; charset=UTF-8
    Content-Transfer-Encoding: 8bit
    
    ## Motivation
    
    CMake configure throws warnings which is annoying. Basically
    https://github.com/ROCm/rocm-libraries/pull/1807, but smaller scope, so
    it can actually go through all code owners 😅
    
    ## Technical Details
    
    Implementation follows suggested upgrade steps from warnings.
    
    ## Test Plan
    
    Modified projects configure and build.
    
    ## Test Result
    
    It really do configure with less warnings now.
    
    ## Submission Checklist
    
    - [x] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- cmake/FindROCMCmake.cmake
+++ cmake/FindROCMCmake.cmake
@@ -1,48 +1,36 @@
 # ########################################################################
-# Copyright 2021 Advanced Micro Devices, Inc.
+# Copyright 2021-2025 Advanced Micro Devices, Inc.
 # ########################################################################
 
-# ###########################
-# rocThrust dependencies
-# ###########################
-
-# For downloading, building, and installing required dependencies
-include(cmake/DownloadProject.cmake)
-
-set(PROJECT_EXTERN_DIR ${CMAKE_CURRENT_BINARY_DIR}/extern)
-
-# By default, rocm software stack is expected at /opt/rocm
-# set environment variable ROCM_PATH to change location
-if(NOT ROCM_PATH)
-  set(ROCM_PATH /opt/rocm)
+if(NOT DEPENDENCIES_FORCE_DOWNLOAD)
+  find_package(ROCmCMakeBuildTools 0.7.3 CONFIG QUIET PATHS "${ROCM_ROOT}")
 endif()
-
-find_package(ROCM 0.7.3 CONFIG QUIET PATHS ${ROCM_PATH} /opt/rocm)
-if(NOT ROCM_FOUND)
-  set(rocm_cmake_tag "master" CACHE STRING "rocm-cmake tag to download")
-  set(rocm_cmake_url "https://github.com/RadeonOpenCompute/rocm-cmake/archive/${rocm_cmake_tag}.zip")
-  set(rocm_cmake_path "${PROJECT_EXTERN_DIR}/rocm-cmake-${rocm_cmake_tag}")
-  set(rocm_cmake_archive "${rocm_cmake_path}.zip")
-  file(DOWNLOAD "${rocm_cmake_url}" "${rocm_cmake_archive}" STATUS status LOG log)
-
-  list(GET status 0 status_code)
-  list(GET status 1 status_string)
-
-  if(status_code EQUAL 0)
-    message(STATUS "downloading... done")
+if(NOT ROCmCMakeBuildTools_FOUND)
+  message(STATUS "ROCm CMake not found. Fetching...")
+  # We don't really want to consume the build and test targets of ROCm CMake.
+  # CMake 3.18 allows omitting them, even though there's a CMakeLists.txt in source root.
+  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.18)
+    set(SOURCE_SUBDIR_ARG SOURCE_SUBDIR "DISABLE ADDING TO BUILD")
   else()
-    message(FATAL_ERROR "error: downloading\n'${rocm_cmake_url}' failed
-    status_code: ${status_code}
-    status_string: ${status_string}
-    log: ${log}\n")
+    set(SOURCE_SUBDIR_ARG)
   endif()
-
-  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzvf "${rocm_cmake_archive}"
-    WORKING_DIRECTORY ${PROJECT_EXTERN_DIR})
-  execute_process( COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${PROJECT_EXTERN_DIR}/rocm-cmake .
-    WORKING_DIRECTORY ${PROJECT_EXTERN_DIR}/rocm-cmake-${rocm_cmake_tag} )
-  execute_process( COMMAND ${CMAKE_COMMAND} --build rocm-cmake-${rocm_cmake_tag} --target install
-    WORKING_DIRECTORY ${PROJECT_EXTERN_DIR})
-
-  find_package( ROCM 0.7.3 REQUIRED CONFIG PATHS ${PROJECT_EXTERN_DIR}/rocm-cmake )
+  include(FetchContent)
+  FetchContent_Declare(
+    rocm-cmake
+    GIT_REPOSITORY https://github.com/ROCm/rocm-cmake.git
+    GIT_TAG        rocm-6.4.4
+    ${SOURCE_SUBDIR_ARG}
+  )
+  FetchContent_MakeAvailable(rocm-cmake)
+  find_package(ROCmCMakeBuildTools CONFIG REQUIRED NO_DEFAULT_PATH PATHS "${rocm-cmake_SOURCE_DIR}")
+else()
+  find_package(ROCmCMakeBuildTools 0.7.3 CONFIG REQUIRED PATHS "${ROCM_ROOT}")
 endif()
+
+include(ROCMSetupVersion)
+include(ROCMCreatePackage)
+include(ROCMInstallTargets)
+include(ROCMPackageConfigHelpers)
+include(ROCMInstallSymlinks)
+include(ROCMCheckTargetIds)
+include(ROCMClients)
