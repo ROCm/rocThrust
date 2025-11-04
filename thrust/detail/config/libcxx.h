357f249895ac0e5885d09dea2a48c741a5f513d1
Nara <nara@streamhpc.com>
2025-11-04 20:12:12 +0100

    fix(rocthrust): add '__has_include' guards for cccl include headers that are introduced with cccl 2.8 (#2430)
    ## Motivation
    
    **This change also needs to be backported to 7.1.x**
    
    Certain libhipcxx files are only available on CCCL 2.8. To improve
    compatibility with older CCCL versions we can introduce a more lenient
    include guard that only loads these includes if they're available.
    
    ## Technical Details
    
    `_THRUST_STD_INCLUDE` modifies the given include path to point to the
    right backing library. We can also use that to check for include headers
    via `__has_include`.
    
    ## Test Plan
    
    Testing should be handled internally.
    
    ## Test Result
    
    Works on my machine.
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- thrust/detail/config/libcxx.h
+++ thrust/detail/config/libcxx.h
@@ -33,29 +33,45 @@
 //     #include _THRUST_STD_INCLUDE(optional)
 //     using optional_int = _THRUST_STD::optional<int>;
 
-// If 'libcudacxx' is available
-// clang-format off
-#if THRUST_HAS_INCLUDE(<cuda/std/cassert>)
-// clang-format on
-#  define _THRUST_LIBCXX_INCLUDE(LIB)   <cuda/LIB>
-#  define _THRUST_STD_INCLUDE(LIB)      <cuda/std/LIB>
-#  define _THRUST_LIBCXX                ::cuda
-#  define _THRUST_STD                   _CUDA_VSTD
-#  define _THRUST_HAS_DEVICE_SYSTEM_STD 1
-#  define _THRUST_STD_NAMESPACE_BEGIN   _LIBCUDACXX_BEGIN_NAMESPACE_STD
-#  define _THRUST_STD_NAMESPACE_END     _LIBCUDACXX_END_NAMESPACE_STD
+// Version that we depend on. We can ignore patch for now
+// since we're only interested in breaking (major) and
+// features (minor).
+#define _THRUST_REQUIRED_LIBCXX_VERSION_MAJOR 2
+#define _THRUST_REQUIRED_LIBCXX_VERSION_MINOR 8
 
-// If 'libhipcxx' is available
-// clang-format off
-#elif THRUST_HAS_INCLUDE(<hip/std/cassert>)
-// clang-format on
-#  define _THRUST_LIBCXX_INCLUDE(LIB)   <hip/LIB>
-#  define _THRUST_STD_INCLUDE(LIB)      <hip/std/LIB>
-#  define _THRUST_LIBCXX                ::hip
-#  define _THRUST_STD                   ::hip::std
-#  define _THRUST_HAS_DEVICE_SYSTEM_STD 1
-#  define _THRUST_STD_NAMESPACE_BEGIN   _LIBCUDACXX_BEGIN_NAMESPACE_STD
-#  define _THRUST_STD_NAMESPACE_END     _LIBCUDACXX_END_NAMESPACE_STD
+// If the '::cuda::std' namespace from 'libcudacxx' or 'libhipcxx' is available. 
+#if THRUST_HAS_INCLUDE(<cuda/std/version>)
+#  include <cuda/std/version>
+// If version matches and '_CUDA_VSTD' is available.
+#  if _LIBCUDACXX_CUDA_API_VERSION_MAJOR == _THRUST_REQUIRED_LIBCXX_VERSION_MAJOR \
+    && _LIBCUDACXX_CUDA_API_VERSION_MINOR >= _THRUST_REQUIRED_LIBCXX_VERSION_MINOR \
+    && defined (_CUDA_VSTD)
+#    define _THRUST_LIBCXX_INCLUDE(LIB)   <cuda/LIB>
+#    define _THRUST_STD_INCLUDE(LIB)      <cuda/std/LIB>
+#    define _THRUST_LIBCXX                ::cuda
+#    define _THRUST_STD                   _CUDA_VSTD
+#    define _THRUST_HAS_DEVICE_SYSTEM_STD 1
+#    define _THRUST_STD_NAMESPACE_BEGIN   _LIBCUDACXX_BEGIN_NAMESPACE_STD
+#    define _THRUST_STD_NAMESPACE_END     _LIBCUDACXX_END_NAMESPACE_STD
+#  endif
+
+// Otherwise, if the '::hip::std' namespace from 'libhipcxx' is available.
+#elif THRUST_HAS_INCLUDE(<hip/std/version>)
+#  include <hip/std/version>
+// If version matches and '_CUDA_VSTD' is available.
+#  if _LIBCUDACXX_CUDA_API_VERSION_MAJOR == _THRUST_REQUIRED_LIBCXX_VERSION_MINOR \
+    && _LIBCUDACXX_CUDA_API_VERSION_MINOR >= _THRUST_REQUIRED_LIBCXX_VERSION_MINOR \
+    && defined(_CUDA_VSTD)
+#    define _THRUST_LIBCXX_INCLUDE(LIB)   <hip/LIB>
+#    define _THRUST_STD_INCLUDE(LIB)      <hip/std/LIB>
+// In 'libhipcxx' the '::hip' namespace is synonymous with '::cuda'. 
+#    define _THRUST_LIBCXX                ::hip
+// In 'libhipcxx' the macro '_CUDA_VSTD' is also defined.
+#    define _THRUST_STD                   _CUDA_VSTD
+#    define _THRUST_HAS_DEVICE_SYSTEM_STD 1
+#    define _THRUST_STD_NAMESPACE_BEGIN   _LIBCUDACXX_BEGIN_NAMESPACE_STD
+#    define _THRUST_STD_NAMESPACE_END     _LIBCUDACXX_END_NAMESPACE_STD
+#  endif
 #endif
 
 // If 'libcudacxx' or 'libhipcxx' is not found, use fallback.
