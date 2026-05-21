f6a0adb8057bd803533a1129ac5d88fd4b7fb292
Wayne Franz <Wayne.Franz@amd.com>
2026-05-21 20:37:00 +0000

    [rocm-libraries] ROCm/rocm-libraries#7343 (commit 0cdc9fa)
    [rocThrust] Disable large size tests under ASAN
    
    ## Motivation
    
    These two rocThrust unit tests use large input sizes, making them
    impractical to run under ASAN:
    - UniqueByKeyIntegralTests.TestUniqueCopyByKeyLargeInput
    - UniqueByKeyIntegralTests.TestUniqueCopyByKeyLargeOutCount
    
    ## Technical Details
    
    This change disables these tests within ASAN builds.
    
    ## Test Plan
    
    Build and run test target unique_by_key.hip with ASAN enabled. Ensure
    the two tests mentioned above are skipped.
    
    ## Test Result
    
    Tests are skipped correctly.
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- test/test_unique_by_key.cpp
+++ test/test_unique_by_key.cpp
@@ -537,6 +537,10 @@ TEST(UniqueIntegralTests, TestUniqueDevice)
 
 TYPED_TEST(UniqueByKeyIntegralTests, TestUniqueCopyByKeyLargeInput)
 {
+#ifdef ADDRESS_SANITIZER_BUILD
+  GTEST_SKIP() << "Skipping large size test for address sanitizer build.";
+#endif
+
   using K          = typename TestFixture::input_type;
   using type       = K;
   using index_type = std::int64_t;
@@ -568,6 +572,10 @@ TYPED_TEST(UniqueByKeyIntegralTests, TestUniqueCopyByKeyLargeInput)
 
 TYPED_TEST(UniqueByKeyIntegralTests, TestUniqueCopyByKeyLargeOutCount)
 {
+#ifdef ADDRESS_SANITIZER_BUILD
+  GTEST_SKIP() << "Skipping large size test for address sanitizer build.";
+#endif
+
   SCOPED_TRACE(testing::Message() << "with device_id= " << test::set_device_from_ctest());
 
   constexpr std::size_t num_items = 4400000000ULL;
