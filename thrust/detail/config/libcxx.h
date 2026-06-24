9ed70390de4b4ff46b8bfa5cf63eef17c0d549e2
Stanley Tsang <stanley.tsang@amd.com>
2026-06-24 20:36:09 +0000

    [rocm-libraries] ROCm/rocm-libraries#8113 (commit 12ebf35)
    [rocThrust] Add feature flag for libhipcxx/libcudacxx backend
     usage (#8113)
    
    ## Motivation
    
    As per
    https://github.com/ROCm/TheRock/blob/main/docs/development/flags.md, new
    features should be flag guarded. The inclusion of libhipcxx usage should
    be one of those cases.
    
    ## Technical Details
    
    There are now two cmake flags which control which backend rocThrust
    uses:
    
    1. ROCTHRUST_USE_LIBHIPCXX
    2. ROCTHRUST_USE_LIBCUDACXX
    
    To use the old rocThrust backend, both flags should be set OFF.
    
    ## Test Plan
    
    Ran the build locally, flags work as expected.
    
    ## Test Result
    
    Tests pass.
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- thrust/detail/config/libcxx.h
+++ thrust/detail/config/libcxx.h
@@ -40,7 +40,8 @@
 #define _THRUST_REQUIRED_LIBCXX_VERSION_MINOR 8
 
 // If the '::cuda::std' namespace from 'libcudacxx' or 'libhipcxx' is available. 
-#if THRUST_HAS_INCLUDE(<cuda/std/version>)
+#if THRUST_HAS_INCLUDE(<cuda/std/version>) \
+	 && (defined(USE_LIBCUDACXX) || defined(USE_LIBHIPCXX))
 #  include <cuda/std/version>
 // If version matches and '_CUDA_VSTD' is available.
 #  if _LIBCUDACXX_CUDA_API_VERSION_MAJOR == _THRUST_REQUIRED_LIBCXX_VERSION_MAJOR \
@@ -56,7 +57,7 @@
 #  endif
 
 // Otherwise, if the '::hip::std' namespace from 'libhipcxx' is available.
-#elif THRUST_HAS_INCLUDE(<hip/std/version>)
+#elif THRUST_HAS_INCLUDE(<hip/std/version>) && defined(USE_LIBHIPCXX)
 #  include <hip/std/version>
 // If version matches and '_CUDA_VSTD' is available.
 #  if _LIBCUDACXX_CUDA_API_VERSION_MAJOR == _THRUST_REQUIRED_LIBCXX_VERSION_MINOR \
@@ -74,8 +75,9 @@
 #  endif
 #endif
 
-// If 'libcudacxx' or 'libhipcxx' is not found, use fallback.
-#ifndef _THRUST_HAS_DEVICE_SYSTEM_STD
+// If 'libcudacxx' or 'libhipcxx' is not found, or the
+// feature flag ROCTHRUST_USE_LIBHIPCXX is not ON, use fallback.
+#ifndef _THRUST_HAS_DEVICE_SYSTEM_STD 
 #  define _THRUST_LIBCXX_INCLUDE(LIB)
 #  define _THRUST_STD_INCLUDE(LIB) <LIB>
 #  define _THRUST_LIBCXX
